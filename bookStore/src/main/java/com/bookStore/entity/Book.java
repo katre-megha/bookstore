bf1f7797c6adda6c933b90a9c32c69af66f025ab
katre-megha <110730934+katre-megha@users.noreply.github.com>
2024-02-11 17:14:27 +0530

    Add files via upload

---
+++ bookStore/src/main/java/com/bookStore/entity/Book.java
@@ -0,0 +1,56 @@
+package com.bookStore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String author;
+    private String price;
+
+    // Default no-argument constructor
+    public Book() {
+        // Optional: You can keep it empty or add any additional logic if needed
+    }
+
+    // Getters and setters...
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
